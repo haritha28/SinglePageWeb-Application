eeaa30cd73acfbbb84f0b21e86fa2513e6e871a1
haritha28 <haritha.h20@gmail.com>
2016-09-19 15:57:39 +0530

    Started Week2 on AngularFilters

---
+++ filters/app.js
@@ -0,0 +1,13 @@
+(function () {
+'use strict';
+
+	angular.module('myApp', [])
+	.controller('personCtrl', function($scope){
+		$scope.firstName = "Hazel"
+		$scope.lastName = "Garderner"
+	})
+
+
+})();
+
+
